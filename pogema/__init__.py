e11646200f9f6fd022b602dc0a248ebc7ab0a055
tviskaron <tviskaron@gmail.com>
2022-03-25 17:36:17 +0300

    added max_episode_steps for predefined envs

--- pogema/__init__.py
+++ pogema/__init__.py
@@ -17,12 +17,13 @@ def _get_num_agents_by_target_density(_size, _agent_density, _obstacle_density):
     return round(_agent_density * (_size * _size * (1.0 - _obstacle_density)))
 
 
-for size in [8, 16, 32, 64]:
+for size, max_episode_steps in zip([8, 16, 32, 64], [64, 128, 256, 512]):
     for obstacle_density in [0.3]:
-        for difficulty, agent_density in zip(['easy', 'normal', 'hard', 'very-hard'], [0.0223, 0.0446, 0.0892, 0.1784]):
+        for difficulty, agent_density in zip(['easy', 'normal', 'hard', 'extra-hard'], [0.0223, 0.0446, 0.0892, 0.1784]):
             num_agents = _get_num_agents_by_target_density(size, agent_density, obstacle_density)
             register(
                 id=f'Pogema-{size}x{size}-{difficulty}-v0',
                 entry_point="pogema.integrations.make_pogema:make_pogema",
-                kwargs={"grid_config": GridConfig(size=size, num_agents=num_agents, density=obstacle_density),
+                kwargs={"grid_config": GridConfig(size=size, num_agents=num_agents, density=obstacle_density,
+                                                  max_episode_steps=max_episode_steps),
                         "integration": None})
