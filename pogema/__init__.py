e9c6ef6f3436ff1b17da66f97bcdc933b4e82b29
tviskaron <tviskaron@gmail.com>
2022-04-05 18:48:26 +0300

    hotfix & better tests

--- pogema/__init__.py
+++ pogema/__init__.py
@@ -9,7 +9,7 @@ __all__ = [
 
 register(
     id="Pogema-v0",
-    entry_point="pogema.make_pogema:make_pogema",
+    entry_point="pogema.integrations.make_pogema:make_pogema",
 )
 
 
@@ -19,7 +19,8 @@ def _get_num_agents_by_target_density(_size, _agent_density, _obstacle_density):
 
 for size, max_episode_steps in zip([8, 16, 32, 64], [64, 128, 256, 512]):
     for obstacle_density in [0.3]:
-        for difficulty, agent_density in zip(['easy', 'normal', 'hard', 'extra-hard'], [0.0223, 0.0446, 0.0892, 0.1784]):
+        for difficulty, agent_density in zip(['easy', 'normal', 'hard', 'extra-hard'],
+                                             [0.0223, 0.0446, 0.0892, 0.1784]):
             num_agents = _get_num_agents_by_target_density(size, agent_density, obstacle_density)
             register(
                 id=f'Pogema-{size}x{size}-{difficulty}-v0',
