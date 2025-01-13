340a38626456fb9dd36689c989d6a33c0f7f919c
tviskaron <tviskaron@gmail.com>
2025-01-13 13:28:18 +0300

    Added method get_full_history for full for PersistentWrapper

--- pogema/__init__.py
+++ pogema/__init__.py
@@ -4,7 +4,7 @@ from pogema.integrations.make_pogema import pogema_v0
 from pogema.svg_animation.animation_wrapper import AnimationMonitor, AnimationConfig
 from pogema.a_star_policy import AStarAgent, BatchAStarAgent
 
-__version__ = '1.3.2a0'
+__version__ = '1.3.2a1'
 
 __all__ = [
     'GridConfig',
