b239585fb36e41472ff14ac93c89bf0ca139d9b4
aandreychuk <andreychuk@mail.com>
2025-06-09 20:05:34 +0300

    Catched one more wrong config case

--- pogema/grid_config.py
+++ pogema/grid_config.py
@@ -109,6 +109,9 @@ class GridConfig(CommonSettings, ):
                         raise ValueError("Each agent must have at least two goals in the sequence")
                     cls.check_positions(agent_goals, width, height)
             else:
+                on_target = values.get('on_target', 'finish')
+                if on_target == 'restart':
+                    raise ValueError("on_target='restart' requires goal sequences, not single goals. Use format: targets_xy=[[x1,y1],[x2,y2]], [[x3,y3],[x4,y4]]")
                 cls.check_positions(v, width, height)
         return v
 
